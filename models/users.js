6f428f14f593b607baa79acfd1f234864f214d88
agepere <angperllo@alum.us.es>
2019-04-16 10:56:34 +0200

    Ala el modelito

---
+++ models/users.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+LibraryItemSchema = new Schema({
+  author: {type: String, required: true},
+  title: {type: String, required: true},
+  description: {type: String},
+  mark: {type: String, enum: ['masterpiece', 'great', 'good', 'bad', 'horrible']}
+
+
+});
+
+
+UserSchema = new Schema({
+
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  username: {type: String, required: true, unique: true, minlength: 3},
+  password: {type: String, required: true, minlength: 3},
+  dni: {type: String},
+
+  music: [LibraryItemSchema],
+  films: [LibraryItemSchema],
+  videogames: [LibraryItemSchema]
+
+});
