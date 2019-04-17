505c1cb9d85afb1c99b53ee80d97c367be4f4ae6
agepere <angperllo@alum.us.es>
2019-04-17 09:19:50 +0200

    get my library, se puede hacer ?filter=music&filter=videogames por ejemplo, para filtrar por música y videojuegos

--- controllers/libraryController.js
+++ controllers/libraryController.js
@@ -1,2 +1,29 @@
 const Users = require('../models/users');
 const ErrorHandler = require('../controllers/errorController');
+const AuthController = require('./authencationController');
+
+
+exports.getMyLibrary = function (req, res) {
+
+  var libraries = "";
+  if (req.query.filter && req.query.filter.length) {
+    for (var x in req.query.filter)
+      if (req.query.filter[x] === "music" || req.query.filter[x] === "films" || req.query.filter[x] === "videogames")
+        libraries += req.query.filter[x].toString() + " ";
+  } else if (req.query.filter && (req.query.filter === "music" || req.query.filter === "films" || req.query.filter === "videogames"))
+    libraries = req.query.filter;
+  else
+    libraries = "music films videogames";
+
+  AuthController.principalUtils(req, user => {
+    Users.findOne({username: user.username}, libraries, (err, data) => {
+      if (err)
+        res.status(400).json(ErrorHandler.handleError(err, null));
+      else if (data === null)
+        res.status(400).json(ErrorHandler.handleError(null, "You're not logged in or you have no libraries"));
+      else
+        res.json(data);
+    })
+  });
+
+};
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const AuthController = require('../controllers/authencationController');
+const LibraryController = require('../controllers/libraryController');
 
 
 // ########## LOGIN ################
@@ -8,6 +9,10 @@ router.get('/login', AuthController.login);
 router.get('/logout', AuthController.logut);
 router.get('/getPrincipal', AuthController.principal);
 
+
+// ############### LIBRARY ##############
+
+router.get('/user/library', LibraryController.getMyLibrary);
 
 module.exports = router;
 
