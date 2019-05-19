ac216d2f3d5780bacb3e04ed222054ec85452ae2
agepere <angperllo@alum.us.es>
2019-05-19 16:13:42 +0200

    Añadida cnfiguración en angular pa que no reviente al recargar la pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {LogoutComponent} from './logout/logout.component';
 import {LoginComponent} from './login/login.component';
 import {UserLibraryComponent} from './library/user-library/user-library.component';
@@ -9,19 +9,20 @@ import {FilterItemComponent} from './library/filter-item/filter-item.component';
 import {PopupFilteredItemsComponent} from './library/popup-filtered-items/popup-filtered-items.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/library/search', pathMatch: 'full'},
-  //User
-  { path: 'user/library', component: UserLibraryComponent},
-  { path: 'user/library/create', component: NewItemComponent},
-  { path: 'user/library/edit/:itemId', component: EditItemComponent},
-  { path: 'library/search', component: FilterItemComponent},
+  {path: '', redirectTo: '/library/search', pathMatch: 'full'},
+  // User
+  {path: 'user/library', component: UserLibraryComponent},
+  {path: 'user/library/create', component: NewItemComponent},
+  {path: 'user/library/edit/:itemId', component: EditItemComponent},
+  {path: 'library/search', component: FilterItemComponent},
   // Login / Logout
-  { path: 'login', component: LoginComponent },
-  { path: 'logout', component: LogoutComponent },
+  {path: 'login', component: LoginComponent},
+  {path: 'logout', component: LogoutComponent},
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true, onSameUrlNavigation: 'reload', scrollPositionRestoration: 'enabled'})],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
